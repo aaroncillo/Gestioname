506eebaee2746e8341b83490b2c93ac8cc17262a
cecilia reynoso <cecireynoso@gmail.com>
2023-06-24 15:13:00 -0300

    nada que push

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,4 +2,18 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.all
   end
+
+  def show
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def edit
+  end
+
+
+
+
 end
