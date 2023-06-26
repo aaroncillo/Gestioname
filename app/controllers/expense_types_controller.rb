19499dbe31d95275dc4e0b61cd6c9e46604de91d
Aaron Deik <aaron.deik.rebora@gmail.com>
2023-06-26 18:45:05 -0400

    ndda

---
+++ app/controllers/expense_types_controller.rb
@@ -0,0 +1,52 @@
+class ExpenseTypesController < ApplicationController
+  before_action :set_expense_type, only: [:edit, :update, :destroy]
+
+  # GET /expense_types
+  def index
+    @expense_types = ExpenseType.where(company_id: params[:company_id])
+  end
+
+  # GET /expense_types/new
+  def new
+    @expense_type = ExpenseType.new
+  end
+
+  # GET /expense_types/1/edit
+  def edit
+  end
+
+  # POST /expense_types
+  def create
+    @expense_type = ExpenseType.new(expense_type_params)
+    if @expense_type.save
+      redirect_to @expense_type, notice: "Expense type was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /expense_types/1
+  def update
+    if @expense_type.update(expense_type_params)
+      redirect_to @expense_type, notice: "Expense type was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /expense_types/1
+  def destroy
+    @expense_type.destroy
+    redirect_to expense_types_url, notice: "Expense type was successfully destroyed."
+  end
+
+  private
+
+  def set_expense_types
+    @expense_type = ExpenseType.find(params[:id])
+  end
+
+  def expense_type_params
+    params.require(:expense_type).permit(:expense_category)
+  end
+end
